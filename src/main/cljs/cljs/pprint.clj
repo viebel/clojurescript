64de2b15d322b963d831f6a15605e47194fde1e0
Yehonathan Sharvit <viebel@gmail.com>
2016-10-07 09:21:57 +0300

    workaround pprint macroexpand

--- src/main/cljs/cljs/pprint.clj
+++ src/main/cljs/cljs/pprint.clj
@@ -7,7 +7,7 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns cljs.pprint
-  (:refer-clojure :exclude [deftype])
+  (:refer-clojure :exclude [deftype macroexpand])
   (:require [clojure.walk :as walk]))
 
 
